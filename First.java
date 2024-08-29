bb6b7f7b2802ff73e5eea3861931b56cf83b7593
GojiyaDev <DEV.17103@stu.upes.ac.in>
2024-08-29 21:34:58 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,35 @@
+/**
+class HelloUPES{
+  public static void main(String[] args){
+  System.out.println("Hello UPES, How is the Josh?");
+  }
+}*/
+
+//Size field in Wrapper Class
+/**
+class size{
+  public static void main(String[] args){
+    System.out.println("char " + Character.SIZE);
+    System.out.println("byte " + Byte.SIZE);
+    System.out.println("short " + Short.SIZE);
+    System.out.println("int " + Integer.SIZE);
+    System.out.println("long " + Long.SIZE);
+    System.out.println("float " + Float.SIZE);
+    System.out.println("double " + Double.SIZE);
+    }
+}*/
+
+//Methods for Parsing
+/**
+class _parse{
+    public static void main(String[] args){
+        String str = "45";
+        String str1 = "true";
+        int a = Integer.parseInt(str);
+        double d = Double.parseDouble(str);
+        boolean b1 = Boolean.parseBoolean(str1);
+        boolean b2 = Boolean.parseBoolean(str);
+        System.out.print(a+" "+d+" "+b1+" "+b2);
+    }
+}
+*/
