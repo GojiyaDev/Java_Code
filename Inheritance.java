e92699765cd5ca8f13961390c24b3e75ff59a3a7
GojiyaDev <DEV.17103@stu.upes.ac.in>
2024-09-19 21:46:14 +0530

    Inheritance java

--- Inheritance.java
+++ Inheritance.java
@@ -59,6 +59,90 @@ class MainClass {
     }
 }
 
+// Constructors in Inheritance 
+class A {
+    A() {
+        System.out.println("I am in Class A");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("I am in Class B");
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        B obj = new B();
+    }
+}
+
+// super: to refer to superclass member
+class A {
+    int a = 100, b = 200;
+}
+
+class B extends A {
+    int a = 10, b = 20;
+
+    void display() {
+        System.out.printf("%d %d %d %d", a, b, super.a, super.b);
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display();
+    }
+}
+
+//Method Overriding
+class A {
+    void display() {
+        System.out.println("I am in Class A");
+    }
+}
+
+class B extends A {
+    void display() {
+        System.out.println("I am in Class B");
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display();
+    }
+}
+
+// Method Overriding
+class A {
+    void display() {
+        System.out.println("I am in Class A");
+    }
+}
+
+class B extends A {
+    void display() {
+        super.display(); // Call the superclass method
+        System.out.println("I am in Class B");
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display(); // Calls the display method of B
+    }
+}
+
+
+
+
+
 
 
 
